c0d2d62deecb07410a8bb4e1141d62c44da016e3
awilson53 <awilson53>
2015-02-11 20:50:41 -0500

    Initial commit of .sln files

---
+++ Parker's Body Shop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Parker_s_Body_Shop.MvcApplication" Language="C#" %>
---
+++ Parker's Body Shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Parker_s_Body_Shop.Startup))]
+namespace Parker_s_Body_Shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
