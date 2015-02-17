a0cd48866286d4fa3afa6211c731e54cd9eb3b1e
awilson53 <awilson53>
2015-02-17 07:53:25 -0500

    began work on contact page

---
+++ Parker's Body Shop/Models/ContactViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parker_s_Body_Shop.Models
+{
+    public class ContactViewModel
+    {
+        public string SMTPUrl { get; set; }
+        public string ContactEmail { get; set; }
+        public string SMTPUser { get; set; }
+        public string SMTPPass { get; set; }
+    }
+}
