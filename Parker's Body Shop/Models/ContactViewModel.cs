e1ccf5c685b247dfb1c8ef85ab663ddb77256e64
awilson53 <awilson53>
2015-02-19 06:38:39 -0500

    -Contact Page now includes google map - Wrote DBContext and View for adding contact email addresses

--- Parker's Body Shop/Models/ContactViewModel.cs
+++ Parker's Body Shop/Models/ContactViewModel.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace Parker_s_Body_Shop.Models
 {
     public class ContactViewModel
     {
+        public int ContactViewModelId { get; set; }
         public string SMTPUrl { get; set; }
         public string ContactEmail { get; set; }
         public string SMTPUser { get; set; }
         public string SMTPPass { get; set; }
+    }
+
+    public class ContactViewModelContext : DbContext
+    {
+        public System.Data.Entity.DbSet<Parker_s_Body_Shop.Models.ContactViewModel> ContactViewModels { get; set; }
     }
 }
