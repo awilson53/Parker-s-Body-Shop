529cda60ff7e94cef2a789d7acb9f6e997017c4a
awilson53 <awilson53>
2015-03-06 07:09:13 -0500

    finished contact form and added response page.  smtp is timing out for some reason.

--- Parker's Body Shop/Controllers/HomeController.cs
+++ Parker's Body Shop/Controllers/HomeController.cs
@@ -26,17 +26,50 @@ namespace Parker_s_Body_Shop.Controllers
         {
             try
             {
-                WebMail.SmtpServer = ConfigurationManager.AppSettings["SMTPUrl"];
+                WebMail.SmtpServer = ConfigurationManager.AppSettings["SMTPServer"];
                 WebMail.SmtpPort = Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPort"]);
                 WebMail.UserName = ConfigurationManager.AppSettings["SMPTUser"];
-                WebMail.Password = ConfigurationManager.AppSettings["SMTPPass"];                
+                WebMail.Password = ConfigurationManager.AppSettings["SMTPPass"];
+                WebMail.From = ConfigurationManager.AppSettings["SMTPFromAddress"];
+                WebMail.EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["SMTPUseSSL"]);
+                
+
+                string to = ConfigurationManager.AppSettings["ContactEmail"];
+                string subject = ConfigurationManager.AppSettings["SMTPSubject"];
+                string body = "You have received a message from the website:" + "\n" +
+                    "First Name: " + contactInfo.FirstName.CheckProvided() + "\n" +
+                    "Last Name: " + contactInfo.LastName.CheckProvided() + "\n" +
+                    "Email: " + contactInfo.Email.CheckProvided() + "\n" +
+                    "Phone Number: " + contactInfo.PhoneNumber.CheckProvided() + "\n" +
+                    "Message: " + contactInfo.Message.CheckProvided();
+
+                WebMail.Send(to, subject, body);
 
                 return View();
 
             }
             catch(Exception ex)
             {
+                ViewBag.ErrorMessage = ex.Message;
+                ViewBag.StackTrace = ex.StackTrace;
+                return View();
+            }
+        }
 
+
+    }    
+
+    public static class Extensions
+    {
+        public static string CheckProvided(this String str)
+        {
+            if (str != null && str.Length > 0)
+            {
+                return str;
+            }
+            else
+            {
+                return "Not Provided";
             }
         }
     }
