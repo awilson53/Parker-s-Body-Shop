3fe3cbd037a4283e0952f89597cfdef9e8b6e24d
awilson53 <awilson53>
2015-03-07 07:35:29 -0500

    Updated to use different SMTP sender...still not working though

--- Parker's Body Shop/Controllers/HomeController.cs
+++ Parker's Body Shop/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Helpers;
+using System.Net.Mail;
 using Parker_s_Body_Shop.Models;
 
 namespace Parker_s_Body_Shop.Controllers
@@ -25,25 +27,49 @@ namespace Parker_s_Body_Shop.Controllers
         public ActionResult SubmitContact(ContactRequestViewModel contactInfo)
         {
             try
-            {
-                WebMail.SmtpServer = ConfigurationManager.AppSettings["SMTPServer"];
-                WebMail.SmtpPort = Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPort"]);
-                WebMail.UserName = ConfigurationManager.AppSettings["SMPTUser"];
-                WebMail.Password = ConfigurationManager.AppSettings["SMTPPass"];
-                WebMail.From = ConfigurationManager.AppSettings["SMTPFromAddress"];
-                WebMail.EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["SMTPUseSSL"]);
-                
+            {               
+                var smtp = new SmtpClient
+                {
+                    Host = ConfigurationManager.AppSettings["SMTPServer"],
+                    Port = Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPort"]),
+                    EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["SMTPUseSSL"]),
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(ConfigurationManager.AppSettings["SMPTUser"], ConfigurationManager.AppSettings["SMTPPass"])
+                };
 
-                string to = ConfigurationManager.AppSettings["ContactEmail"];
-                string subject = ConfigurationManager.AppSettings["SMTPSubject"];
-                string body = "You have received a message from the website:" + "\n" +
-                    "First Name: " + contactInfo.FirstName.CheckProvided() + "\n" +
-                    "Last Name: " + contactInfo.LastName.CheckProvided() + "\n" +
-                    "Email: " + contactInfo.Email.CheckProvided() + "\n" +
-                    "Phone Number: " + contactInfo.PhoneNumber.CheckProvided() + "\n" +
-                    "Message: " + contactInfo.Message.CheckProvided();
+                var fromAddress = new MailAddress(ConfigurationManager.AppSettings["SMTPFromAddress"], "Website Contact");
+                var toAddress = new MailAddress(ConfigurationManager.AppSettings["ContactEmail"]);
 
-                WebMail.Send(to, subject, body);
+                using(var message = new MailMessage(fromAddress, toAddress))
+                {
+                    message.Subject = ConfigurationManager.AppSettings["SMTPSubject"];
+                    message.Body = "You have received a message from the website:" + "\n" +
+                        "First Name: " + contactInfo.FirstName.CheckProvided() + "\n" +
+                        "Last Name: " + contactInfo.LastName.CheckProvided() + "\n" +
+                        "Email: " + contactInfo.Email.CheckProvided() + "\n" +
+                        "Phone Number: " + contactInfo.PhoneNumber.CheckProvided() + "\n" +
+                        "Message: " + contactInfo.Message.CheckProvided();
+
+                    smtp.Send(message);
+                }
+
+                //WebMail.SmtpServer = ConfigurationManager.AppSettings["SMTPServer"];
+                //WebMail.SmtpPort = Convert.ToInt32(ConfigurationManager.AppSettings["SMTPPort"]);
+                //WebMail.UserName = ConfigurationManager.AppSettings["SMPTUser"];
+                //WebMail.Password = ConfigurationManager.AppSettings["SMTPPass"];
+                //WebMail.From = ConfigurationManager.AppSettings["SMTPFromAddress"];
+                //WebMail.EnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["SMTPUseSSL"]);                
+                //string to = ConfigurationManager.AppSettings["ContactEmail"];
+                //string subject = ConfigurationManager.AppSettings["SMTPSubject"];
+                //string body = "You have received a message from the website:" + "\n" +
+                //    "First Name: " + contactInfo.FirstName.CheckProvided() + "\n" +
+                //    "Last Name: " + contactInfo.LastName.CheckProvided() + "\n" +
+                //    "Email: " + contactInfo.Email.CheckProvided() + "\n" +
+                //    "Phone Number: " + contactInfo.PhoneNumber.CheckProvided() + "\n" +
+                //    "Message: " + contactInfo.Message.CheckProvided();
+
+                //WebMail.Send(to, subject, body);
 
                 return View();
 
